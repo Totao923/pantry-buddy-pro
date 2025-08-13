897b8269caeb8931fa5cffd59b56e7ac97fc9b8b
user <user@users-MacBook-Pro.local>
2025-08-13 12:23:04 -0400

    Fix Prettier formatting in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,19 +6,22 @@ export default function Document() {
       <Head>
         {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
-        
+
         {/* Favicons */}
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        
+
         {/* Meta tags */}
         <meta name="theme-color" content="#3b82f6" />
-        <meta name="description" content="Transform your pantry ingredients into restaurant-quality recipes with advanced AI" />
-        
+        <meta
+          name="description"
+          content="Transform your pantry ingredients into restaurant-quality recipes with advanced AI"
+        />
+
         {/* Apple Touch Icon */}
         <link rel="apple-touch-icon" href="/favicon-32x32.png" />
-        
+
         {/* Preconnect to external domains */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
